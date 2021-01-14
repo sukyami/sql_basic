9cf1eb5d0556a124f8605da0d5543a7293d578f8
sukyami <sukyami@gmail.com>
2021-01-14 13:00:36 -0500

    add

--- 011421_sql.py
+++ 011421_sql.py
@@ -1,3 +1,7 @@
-import numpy as np
+import numpy as np 
 import pandas as pd 
-import plotly as pyo
+import plotly.offline as pyo 
+
+df = pd.DataFrame(np.random.randn(100,4),columns = 'A B C D'.split())
+
+pyo.plot([{'x':df.index,'y':df[col],'name':col}for col in df.columns])
